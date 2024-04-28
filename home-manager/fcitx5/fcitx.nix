a4a1c0a8162a84718911be04bb63c191507167d2
laomei <git@laomei.me>
2024-04-28 23:35:10 +0000

    fcitx

--- home-manager/fcitx5/fcitx.nix
+++ home-manager/fcitx5/fcitx.nix
@@ -2,9 +2,13 @@
 {
   i18n.inputMethod = {
     enabled = "fcitx5";
+    waylandFrontend =true;
+
+    plasma6Support = true;
     fcitx5.addons = with pkgs; [
-        fcitx5-mozc
         fcitx5-gtk
+        #fcitx5-chinese-addons
     ];
-};
+  };
+
 }
