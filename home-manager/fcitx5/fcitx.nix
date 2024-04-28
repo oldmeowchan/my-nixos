f73d0ca0c290059497750c986b2fa2dce9e1c4f9
laomei <git@laomei.me>
2024-04-28 23:48:21 +0000

    xx

--- home-manager/fcitx5/fcitx.nix
+++ home-manager/fcitx5/fcitx.nix
@@ -1,5 +1,8 @@
 {pkgs,...}:
 {
 i18n.inputMethod.enabled ="fcitx5";
+#config.services.desktopManager.plasma6.enable = true;
+#i18n.inputMethod.fcitx5.plasma6Support = true;
+i18n.inputMethod.fcitx5.addons= with pkgs; [ fcitx5-rime ];
 
 }
