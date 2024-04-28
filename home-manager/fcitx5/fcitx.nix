36cd270d411438d694985bb32694574f28722f1c
laomei <git@laomei.me>
2024-04-28 23:50:04 +0000

    add fcitx5-chinese-addons

--- home-manager/fcitx5/fcitx.nix
+++ home-manager/fcitx5/fcitx.nix
@@ -3,6 +3,6 @@
 i18n.inputMethod.enabled ="fcitx5";
 #config.services.desktopManager.plasma6.enable = true;
 #i18n.inputMethod.fcitx5.plasma6Support = true;
-i18n.inputMethod.fcitx5.addons= with pkgs; [ fcitx5-rime ];
+i18n.inputMethod.fcitx5.addons= with pkgs; [ fcitx5-rime fcitx5-chinese-addons];
 
 }
