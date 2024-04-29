9506d7b31f40b84431519aaf7e544eb76183b096
laomei <git@laomei.me>
2024-04-29 10:38:14 +0000

    add vscode-extensions.ms-vscode.cpptools

--- home-manager/fcitx5/fcitx.nix
+++ home-manager/fcitx5/fcitx.nix
@@ -4,5 +4,5 @@
 {  
   i18n.inputMethod.enabled ="fcitx5";
 
-i18n.inputMethod.fcitx5.addons= with pkgs; [  kdePackages.fcitx5-chinese-addons  fcitx5-gtk ];
+ i18n.inputMethod.fcitx5.addons= with pkgs; [  kdePackages.fcitx5-chinese-addons  fcitx5-gtk ];
 }
--- home-manager/vscode.nix
+++ home-manager/vscode.nix
@@ -5,6 +5,7 @@
   extensions = with pkgs.vscode-extensions; [
     arcticicestudio.nord-visual-studio-code
     vscodevim.vim
+    vscode-extensions.ms-vscode.cpptools
   ];
   };
 }
